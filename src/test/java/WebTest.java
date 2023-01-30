8687fc86359beced134c800e014bac37af7ba91b
Angel123Lara <adjls@gmail.com>
2023-01-29 23:56:49 -0600

    update modify on entire proyect

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -2,14 +2,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import io.opentelemetry.sdk.logs.data.Body;
+
 
 import static org.testng.Assert.assertEquals;
 
